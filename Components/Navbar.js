0163fb82af31429bc8f2ff7f5420145200e1fbdc
chris <Mileschris466@gmail.com>
2025-01-21 12:26:35 +0300

    additions

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -6,7 +6,20 @@ export class Navbar extends Component {
   render() {
     return (
       <div>
-       
+          <nav className="navbar">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link" href="#">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">About</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Contact</a>
+              </li>
+            </ul>
+          </nav>
+  
       </div>
     )
   }
